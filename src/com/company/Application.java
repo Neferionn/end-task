ad291c73c3d83415a1bd127ede7bf5659a243f72
nevrend <anonymperson@mail.ru>
2020-10-02 12:02:30 +0300

    last

--- src/com/company/Application.java
+++ src/com/company/Application.java
@@ -9,21 +9,21 @@ public class Application {
     public static void main(String[] args) {
         List<User> userList = new ArrayList<>();
         userList.add(new User("Vasile","Negru","vasilenegru@gmail.com",20,
-                Timestamp.valueOf(LocalDateTime.now().minusDays(2)),Status.NEW ));
+                Timestamp.valueOf(LocalDateTime.now().minusDays(2)),Status.INACTIVE ));
         userList.add(new User("Ion","Rosca","ionrosca@gmail.com",20,
                 Timestamp.valueOf(LocalDateTime.now().minusDays(4)),Status.NEW ));
         userList.add(new User("Andrei","Brown","andrewbrown@gmail.com",22,
                 Timestamp.valueOf(LocalDateTime.now()),Status.NEW ));
         userList.add(new User("Nikita","Lexus","lexus@gmail.com",20,
-                Timestamp.valueOf(LocalDateTime.now()),Status.INACTIVE ));
+                Timestamp.valueOf(LocalDateTime.now().minusMonths(2)),Status.INACTIVE ));
         userList.add(new User("Alex","Ester","Ester@gmail.com",20,
                 Timestamp.valueOf(LocalDateTime.now()),Status.ACTIVE ));
         userList.forEach((user ->{
 
-            if (user.getTimestamp().before(User.yesterday())){
+            if (user.status==Status.NEW && user.getTimestamp().before(User.yesterday())){
                 user.setStatus(Status.ACTIVE);
             }
-            else if (user.getTimestamp().before(User.month())){
+            if (user.status==Status.INACTIVE && user.getTimestamp().before(User.month())){
                 user.setStatus(Status.BLOCKED);
             }
 
